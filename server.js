f667b8786671ff37264e4ed2a8b7b75d8a991c8a
Scott Bowler <scott.bowler@learningfuze.com>
2019-11-16 22:56:09 +0000

    Setup server with inital get students endpoint

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('./db');
+
+const app = express();
+
+app.get('/api/students', async (req, res) => {
+
+    const result = await db.query('SELECT * FROM grades');
+
+    res.send({
+        message: 'This will contain students',
+        students: result
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server listening @ localhost:3000');
+});
